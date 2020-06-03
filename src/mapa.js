f3ac0b0ea806e2fa9c5b50a33f89d1fbe9cbda35
Mateuszao <mateusgarcia@id.uff.br>
2020-06-03 15:00:38 -0300

    Adicionando as funcionalidades do mapa

---
+++ src/mapa.js
@@ -0,0 +1,7 @@
+function pegaNome() {
+    var endereco = document.getElementById('nome_endereco').value;
+    if (endereco) {
+        alert('Seu endereço é ' + '' + endereco + '. Gostou de testar nosso app? Vamos te redirecionar pra nossa pagira :)');
+        // window.location.replace("http://www.devmedia.com.br"); 
+    }
+}
